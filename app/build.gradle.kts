f51072c2ba0170b76ebd2ff19447f2c0876b0837
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-02-13 17:52:45 +0800

    release: 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.muedsa.tvbox.libvio"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.0.10"
+        versionCode = 11
+        versionName = "0.1.0"
     }
 
     signingConfigs {
